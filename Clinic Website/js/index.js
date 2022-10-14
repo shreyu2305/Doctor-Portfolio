56a3273640fdf993406e02a05e2e742038fccf3d
Shreyash Lodhi <107924705+shreyu2305@users.noreply.github.com>
2022-10-14 16:18:36 +0530

    Add files via upload

---
+++ Clinic Website/js/index.js
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    $(document).ready(function(){
+        $('#icon').click(function(){
+            $('ul').toggleClass('show')
+        })
+    })
